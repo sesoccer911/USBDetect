79a9d29a0802d1bd4cc547b8ccea3972aaa71e68
Steven Green <steven.green@continuant.com>
2019-03-25 08:21:11 -0700

    Add project files.

---
+++ USBDetect/PrimaryForm.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace USBDetect
+{
+    partial class PrimaryForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(288, 95);
+            this.listBox1.TabIndex = 0;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.Location = new System.Drawing.Point(201, 126);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 1;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnRun
+            // 
+            this.btnRun.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRun.Enabled = false;
+            this.btnRun.Location = new System.Drawing.Point(120, 126);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 23);
+            this.btnRun.TabIndex = 2;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            // 
+            // PrimaryForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(288, 161);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.listBox1);
+            this.Name = "PrimaryForm";
+            this.Text = "PrimaryForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnRun;
+    }
+}
+
---
+++ USBDetect/PrimaryForm.cs
@@ -0,0 +1,57 @@
+﻿using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace USBDetect
+{
+    public partial class PrimaryForm : Form
+    {
+        
+        private const int WM_DEVICECHANGE = 0x219;
+        //Tells when devices have been added/removed
+        private const int DBT_DEVICEARRIVAL = 0x8000;
+        private const int DBT_DEVICEREMOVECOMPLETE = 0x8004;
+        //Tells when devices are storage
+        private const int DBT_DEVTYP_VOLUME = 0x00000002;
+
+        public PrimaryForm()
+        {
+            InitializeComponent();
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            //used to handle DEVICEARRIVAL and DEVICEREMOVECOMPLETE
+            base.WndProc(ref m);
+
+            switch (m.Msg)
+            {
+                case WM_DEVICECHANGE:
+                    switch ((int)m.WParam)
+                    {
+                        case DBT_DEVICEARRIVAL:
+                            listBox1.Items.Add("New Device Arrived");
+
+                            int devType = Marshal.ReadInt32(m.LParam, 4);
+                            if (devType == DBT_DEVTYP_VOLUME)
+                            {
+                                DevBroadcastVolume vol;
+                                vol = (DevBroadcastVolume)Marshal.PtrToStructure(m.LParam,
+                                   typeof(DevBroadcastVolume));
+                                listBox1.Items.Add("Mask is " + vol.Mask);
+                            }
+                            break;
+
+                        case DBT_DEVICEREMOVECOMPLETE:
+                            listBox1.Items.Add("Device Removed");
+                            break;
+                    }
+                    break;
+            }
+        }
+
+        private void btnClose_Click(object sender, System.EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
